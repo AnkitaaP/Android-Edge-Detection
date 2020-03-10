3621bd36ed7013a67e7e6d418f461634a63b3331
AnkitaaP <ankita.a.patil123@gmail.com>
2020-03-11 03:55:22 +0530

    Version 1.0 - Edge Detection

---
+++ app/src/main/java/com/example/carddetection/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.carddetection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.*;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+    int counter =0;
+    boolean startCanny = false;
+    public void Canny(View Button){
+        if(startCanny == false){
+            startCanny = true;
+        }
+        else{
+            startCanny = false;
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.cameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+
+        final Button edgeDetector = findViewById(R.id.cameraButton);
+        edgeDetector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Canny(edgeDetector);
+            }
+        });
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                super.onManagerConnected(status);
+                switch (status){
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat frame = inputFrame.rgba();
+
+        if(startCanny == true){
+            //Core.flip(frame, frame, 1);
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
+            Imgproc.blur(frame, frame, new Size(3, 3));
+            Imgproc.Canny(frame, frame, 100, 80);
+        }
+        counter = counter+1;
+        return frame;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Toast.makeText(getApplicationContext(), "There's an error", Toast.LENGTH_LONG ).show();
+        }
+        else{
+            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
+        }
+
+    }
+}
